dcdab9e7a76cdbf8b321d581f51ebf078015071d
jesusmatiasdelgado <jesusmatiasdelgado@gmail.com>
2026-03-27 19:53:07 -0500

    1

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLJM_Numero_del_1_al_10_y_del_10_al_1__for_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            for (int i = 10; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
